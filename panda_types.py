2217d25fa583b958c6673050a9602031585a3f98
tobspr <tobias.springer1@googlemail.com>
2015-05-15 15:39:30 +0200

    Add GeomVertexData, Geom. Add super().__init__() if not defined yet

--- panda_types.py
+++ panda_types.py
@@ -9,7 +9,6 @@ class TypedWritable(TypedObject):
     def write_datagram(self, manager, dg):
         pass
 
-
 class TypedWritableReferenceCount(TypedWritable):
     bam_type_name = "TypedWritableReferenceCount"
 
@@ -18,6 +17,7 @@ class Material(TypedWritableReferenceCount):
     bam_type_name = "Material"
 
     def __init__(self, name=""):
+        super().__init__(self)
         self.name = str(name)
         self.ambient = None
         self.diffuse = None
@@ -45,7 +45,7 @@ class Material(TypedWritableReferenceCount):
             flags |= 0x020
 
         dg.add_vec4(self.ambient or (1, 1, 1, 1))
-        dg.add_vec4(self.diffuse or (0, 0, 0, 1))
+        dg.add_vec4(self.diffuse or (1, 1, 1, 1))
         dg.add_vec4(self.specular or (0, 0, 0, 1))
         dg.add_vec4(self.emission or (0, 0, 0, 1))
 
@@ -69,6 +69,7 @@ class TransformState(TypedWritableReferenceCount):
     bam_type_name = "TransformState"
 
     def __init__(self):
+        super().__init__(self)
         self.pos = None
         self.quat = None
         self.hpr = None
@@ -127,6 +128,7 @@ class PandaNode(TypedWritableReferenceCount):
     bam_type_name = "PandaNode"
 
     def __init__(self, name=""):
+        super().__init__(self)
         self.name = name
 
         self.state = RenderState.empty
@@ -185,6 +187,82 @@ class PandaNode(TypedWritableReferenceCount):
             manager.write_pointer(dg, stashed)
             dg.add_int32(0) # sort
 
+
+class GeomVertexData(TypedWritableReferenceCount):
+    bam_type_name = "GeomVertexData"
+
+    UH_client = 0
+    UH_stream = 1
+    UH_dynamic = 2
+    UH_static = 3
+
+    def __init__(self, name=""):
+        super().__init__(self)
+
+        self.name = name
+        self.format = None
+        self.usage_hint = self.UH_static
+        self.arrays = []
+        self.transform_table = None
+        self.transform_blend_table = None
+        self.slider_table = None
+
+    def write_datagram(self, manager, dg):
+        super().write_datagram(manager, dg)
+
+        dg.add_string(self.name)
+
+        assert self.format
+        # assert isinstance(self.format, GeomVertexFormat) # must be implemented first
+        manager.write_pointer(dg, self.format)
+        dg.add_uint8(self.usage_hint)
+        dg.add_uint16(len(self.arrays))
+        for array in self.arrays:
+            manager.write_pointer(dg, array)
+
+        manager.write_pointer(dg, self.transform_table)
+        manager.write_pointer(dg, self.transform_blend_table)
+        manager.write_pointer(dg, self.slider_table)
+
+class Geom(TypedWritableReferenceCount):
+    bam_type_name = "Geom"
+
+    SM_uniform = 0
+    SM_smooth = 1
+    SM_flat_first_vertex = 2
+    SM_flat_last_vertex = 3
+
+    PT_none = 0
+    PT_polygons = 1
+    PT_lines = 2
+    PT_points = 3
+    PT_patches = 4
+
+    def __init__(self):
+        super().__init__(self)
+        self.data = None
+        self.primitives = []
+        self.primitive_type = self.PT_none
+        self.shade_model = self.SM_smooth
+        self.bounds_type = 0
+
+    def write_datagram(self, manager, dg):
+        super().write_datagram(manager, dg)
+
+        assert self.data
+        assert isinstance(self.data, GeomVertexData)
+        manager.write_pointer(dg, self.data)
+
+        dg.add_uint16(len(self.primitives))
+        for primitive in self.primitives:
+            manager.write_pointer(dg, primitive)
+
+        dg.add_uint8(self.primitive_type)
+        dg.add_uint8(self.shade_model)
+        dg.add_uint16(0) # reserved
+
+        if manager.file_version >= (6, 19):
+            dg.add_uint8(self.bounds_type)
 
 class GeomNode(PandaNode):
     bam_type_name = "GeomNode"
